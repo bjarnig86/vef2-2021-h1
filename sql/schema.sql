aba4dec56fb1006edbcc2c8e672a48e3d174539d
Áslaug Högna <ash10@hi.is>
2021-03-08 23:53:17 +0000

    bætt við í schema, join tables

---
+++ sql/schema.sql
@@ -0,0 +1,69 @@
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id serial primary key,
+  username varchar(32) NOT NULL UNIQUE,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  admin boolean default false
+);
+
+DROP TABLE IF EXISTS categories;
+CREATE TABLE categories (
+  id serial primary key,
+  title varchar(255) NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS shows;
+CREATE TABLE shows (
+  id serial primary key,
+  title varchar(255) NOT NULL,
+  first_aired date,
+  in_production boolean,
+  tagline varchar(255),
+  image varchar(255) NOT NULL,
+  description text,
+  language varchar(2) NOT NULL,
+  network varchar(255),
+  webpage varchar(255)
+);
+
+DROP TABLE IF EXISTS seasons;
+CREATE TABLE seasons (
+  id serial primary key,
+  title varchar(255) NOT NULL,
+  number smallint NOT NULL CHECK (number > 0),
+  first_aired date,
+  description text,
+  poster varchar(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS episodes;
+CREATE TABLE episodes (
+  id serial primary key,
+  title varchar(255) NOT NULL,
+  number smallint NOT NULL CHECK (number > 0),
+  first_aired date,
+  description text
+);
+
+DROP TABLE IF EXISTS shows_categories;
+CREATE TABLE shows_categories (
+  id SERIAL PRIMARY KEY,
+  show INTEGER NOT NULL,
+  category INTEGER NOT NULL,
+  CONSTRAINT show FOREIGN KEY (show) REFERENCES shows (id),
+  CONSTRAINT category FOREIGN KEY (category) REFERENCES categories (id),
+);
+
+DROP TABLE IF EXISTS users_shows;
+CREATE TYPE status AS ENUM('Langar að horfa', 'Er að horfa', 'Hef horft');
+CREATE TABLE users_shows (
+  id SERIAL PRIMARY KEY,
+  show INTEGER NOT NULL,
+  "user" INTEGER NOT NULL,
+  CONSTRAINT show FOREIGN KEY (show) REFERENCES shows (id),
+  CONSTRAINT "user" FOREIGN KEY ("user") REFERENCES users (id),
+  rating INTEGER CHECK (rating between 0 and 5),
+  status status
+);
